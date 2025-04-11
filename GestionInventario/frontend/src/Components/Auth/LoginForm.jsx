ea493459f6876da7510633ecdc4639d0b9650b6a
STEVEN2199 <stevenguillermo_encalada@hotmail.com>
2025-04-10 22:55:55 -0500

    feat: create new api client

--- GestionInventario/frontend/src/Components/Auth/LoginForm.jsx
+++ GestionInventario/frontend/src/Components/Auth/LoginForm.jsx
@@ -1,68 +1,77 @@
-import React, { useState, useContext } from 'react'; // Import useContext
-import axios from 'axios';
-import { useNavigate, Link } from 'react-router-dom';
+import React, { useState, useContext } from "react"; // Import useContext
+import axios from "axios";
+import { useNavigate, Link } from "react-router-dom";
 import { AuthContext } from "../../contexts/AuthContext"; // Import the Context itself
+import apiClient from "@/api/apiClient";
 
 const LoginForm = () => {
-    const [userName, setuserName] = useState('');
-    const [password, setPassword] = useState('');
-    const [error, setError] = useState('');
-    const navigate = useNavigate();
-    const { login } = useContext(AuthContext); // Use useContext to get the login function
+  const [userName, setuserName] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const navigate = useNavigate();
+  const { login } = useContext(AuthContext); // Use useContext to get the login function
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        setError('');
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setError("");
 
-        try {
-            const response = await axios.post('https://localhost:7193/api/Auth/login', { 
-                userName, password 
-            });
-            //const { token } = response.data.message;
-            console.log(response.data.message);
-            login(response.data.message); // Call the login function from the context
-            navigate('/products');
-            console.log('Inicio de sesión exitoso:', response.data);
-        } catch (error) {
-            console.error('Error al iniciar sesión:', error.response ? error.response.data : error.message);
-            setError(error.response?.data?.message || 'Error al iniciar sesión. Por favor, inténtalo de nuevo.');
-        }
-    };
+    try {
+      const response = await apiClient.post("/Auth/login", {
+        userName,
+        password,
+      });
+      //const { token } = response.data.message;
+      console.log(response.data.message);
+      login(response.data.message); // Call the login function from the context
+      navigate("/products");
+      console.log("Inicio de sesión exitoso:", response.data);
+    } catch (error) {
+      console.error(
+        "Error al iniciar sesión:",
+        error.response ? error.response.data : error.message
+      );
+      setError(
+        error.response?.data?.message ||
+          "Error al iniciar sesión. Por favor, inténtalo de nuevo."
+      );
+    }
+  };
 
-    return (
+  return (
+    <div>
+      <h2>Iniciar Sesión</h2>
+      {error && <p style={{ color: "red" }}>{error}</p>}
+      <form onSubmit={handleSubmit}>
         <div>
-            <h2>Iniciar Sesión</h2>
-            {error && <p style={{ color: 'red' }}>{error}</p> }
-            <form onSubmit={handleSubmit}>
-                <div>
-                    <label htmlFor="userName">Nombre Usuario:</label>
-                    <input
-                        type="text"
-                        id="userName"
-                        value={userName}
-                        onChange={(e) => setuserName(e.target.value)}
-                        required
-                    />
-                </div>
-                <div>
-                    <label htmlFor="password">Contraseña:</label>
-                    <input
-                        type="password"
-                        id="password"
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                        required
-                    />
-                </div>
-                <button type="submit">Iniciar Sesión</button>
-            </form>
-            <p>¿No tienes una cuenta? <Link to="/register">Regístrate aquí</Link></p>
-            <p>
-                <Link to="/forgot-password">¿Olvidaste tu contraseña?</Link>
-            </p>
+          <label htmlFor="userName">Nombre Usuario:</label>
+          <input
+            type="text"
+            id="userName"
+            value={userName}
+            onChange={(e) => setuserName(e.target.value)}
+            required
+          />
         </div>
-    );
+        <div>
+          <label htmlFor="password">Contraseña:</label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </div>
+        <button type="submit">Iniciar Sesión</button>
+      </form>
+      <p>
+        ¿No tienes una cuenta? <Link to="/register">Regístrate aquí</Link>
+      </p>
+      <p>
+        <Link to="/forgot-password">¿Olvidaste tu contraseña?</Link>
+      </p>
+    </div>
+  );
 };
-
 
 export default LoginForm;
